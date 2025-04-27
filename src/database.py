b9e303907fdd1a92a2068da5336d56712397e105
AmanDevelops <amanpal2072004@gmail.com>
2025-04-27 23:49:52 +0530

    Refactor database query functions to improve readability and maintainability

--- src/database.py
+++ src/database.py
@@ -1,6 +1,22 @@
 import psycopg2
 
 connection = psycopg2.connect(host="localhost", dbname="qr-ticketing", user="postgres", password="root", port=5432)
-
-
 cursor = connection.cursor()
+
+def read_query(query: str) -> psycopg2.extensions.cursor:
+    """
+    Args:
+        query(str): processes query to the database
+    """
+    cursor.execute(query)
+    return cursor
+
+
+def write_query(query: str) -> psycopg2.extensions.cursor:
+    """
+    Args:
+        query(str): writes query to the database
+    """
+    cursor.execute(query)
+    connection.commit()
+    return cursor
